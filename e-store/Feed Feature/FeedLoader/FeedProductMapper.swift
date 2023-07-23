e2fc3fc348a7223f8016756024aab4903ca0326e
christian.sonic <yoh.chrisitan@gmail.com>
2023-07-23 17:11:54 -0400

    update the model decoder

--- e-store/Feed Feature/FeedLoader/FeedProductMapper.swift
+++ e-store/Feed Feature/FeedLoader/FeedProductMapper.swift
@@ -12,6 +12,17 @@ enum FeedMapper {
         guard let root = try? JSONDecoder().decode([FeedProduct].self, from: data) else {
             return .failure(LocalFeedLoader.Error.invalidData)
         }
-        return .success(root.map{ ProductModel(id: $0.id, url: $0.url, name: $0.name, description: $0.description, terms: $0.terms, current_value: $0.current_value)})
+        return .success(
+            root.map {
+                ProductModel(
+                    id: $0.id ?? "",
+                    url: $0.url ?? "",
+                    name: $0.name ?? "",
+                    description: $0.description ?? "",
+                    terms: $0.terms ?? "",
+                    current_value: $0.current_value ?? ""
+                )
+            }
+        )
     }
 }
--- e-store/Feed Feature/FeedProduct.swift
+++ e-store/Feed Feature/FeedProduct.swift
@@ -7,40 +7,13 @@
 
 import Foundation
 
-struct FeedProduct: Decodable, Equatable {
-    
-    let id: String
-    let url: String
-    let name: String
-    let description: String
-    let terms: String
-    let current_value: String
-    
-    enum CodingKeys: CodingKey {
-        case id
-        case url
-        case name
-        case description
-        case terms
-        case current_value
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decodeIfPresent(String.self, forKey: .id) ?? ""
-        self.url = try container.decodeIfPresent(String.self, forKey: .url) ?? ""
-        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
-        self.description = try container.decodeIfPresent(String.self, forKey: .description) ?? ""
-        self.terms = try container.decodeIfPresent(String.self, forKey: .terms) ?? ""
-        self.current_value = try container.decodeIfPresent(String.self, forKey: .current_value) ?? ""
-    }
-    
-    init(id: String, url: String, name: String, description: String, terms: String, current_value: String) {
-        self.id = id
-        self.url = url
-        self.name = name
-        self.description = description
-        self.terms = terms
-        self.current_value = current_value
-    }
+import Foundation
+
+struct FeedProduct: Codable, Equatable {
+    let id: String?
+    let url: String?
+    let name: String?
+    let description: String?
+    let terms: String?
+    let current_value: String?
 }
